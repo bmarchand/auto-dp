4d4b5b38af37a0e8855e82b4d8254b24222a373d
Bertrand Marchand <bertrand.marchand@lix.polytechnique.fr>
2022-04-26 10:45:38 +0200

    first commit - tree decompositions of HKLM, the 4 1-shadows

---
+++ workflow/scripts/dbn_to_gr.py
@@ -0,0 +1,28 @@
+
+symbols = ['()','[]','{}','<>','Aa','Bb','Cc']
+
+stacks = {}
+
+for s in symbols:
+    stacks[s] = []
+
+dbn = open(snakemake.input[0]).readlines()[0]
+
+vertices = set([])
+edges = set([])
+
+for k,c in enumerate(dbn):
+    vertices.add(k+1)
+    if k < len(dbn)-1:
+        edges.add((k+1,k+2))
+    for s in symbols:
+        if c==s[0]:
+            stacks[s].append(k+1)
+        if c==s[1]:
+            edges.add((stacks[s].pop(), k+1))
+
+f = open(snakemake.output[0], 'w')
+
+f.write('p tw '+str(len(vertices))+' '+str(len(edges))+'\n')
+for u,v in edges:
+    print(u,v,file=f)
